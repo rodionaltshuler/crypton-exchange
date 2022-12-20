c91f1a38e796047ea4caaaa43a307f51a1fa118e
Rodion Altshuler <altshuler.rodion@gmail.com>
2022-12-20 22:46:15 +0100

    Exchange transaction: 1. model for ExchangeTx and Wallet 2. application of ExchangeTx to participating wallets

---
+++ wallet-streams/src/main/java/org/example/Wallet.kt
@@ -0,0 +1,14 @@
+package org.example
+
+data class Wallet(val walletId: String, val assets: Map<String, Asset>) {
+    companion object {
+        @JvmStatic
+        fun withAsset(walletId: String, assetId: String, amount: Double, blockedAmount: Double): Wallet {
+            val asset = Asset(assetId, amount, blockedAmount)
+            val assets = java.util.Map.of(assetId, asset)
+            return Wallet(walletId, assets)
+        }
+    }
+}
+
+data class Asset(val assetId: String, val amount: Double, val blocked: Double)
