29704d83fd7d9574142aebc5fbf7e9b5fba699b8
Rodion Altshuler <rodion.altshuler@gmail.com>
2023-01-07 13:45:35 +0100

    REST API module (Spring + Kotlin)

---
+++ api/src/test/kotlin/com/crypton/exchange/api/ApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.crypton.exchange.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
